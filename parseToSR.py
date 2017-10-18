b59b50539c4a0621eceedb33a47545e297a9af0e
lizhaode <308697716@qq.com>
2017-10-18 07:09:29 -0500

    Update parseToSR.py

--- parseToSR.py
+++ parseToSR.py
@@ -1,9 +1,38 @@
+
 if __name__ == '__main__':
     tmp = []
-    with open('ipReject.txt') as f:
+    # 生成CIDR广告过滤
+    # with open('ipReject.txt') as f:
+    #     for i in f.readlines():
+    #         i = i.strip('\n')
+    #         final = 'IP-CIDR,{ip},REJECT\n'.format(ip=i)
+    #         tmp.append(final)
+    # with open('reject.conf','w') as f:
+    #     f.writelines(tmp)
+
+    # 生成域名代理
+    # with open('domainProxy.txt') as f:
+    #     for i in f.readlines():
+    #         i = i.strip('\n')
+    #         if i.endswith('$'):
+    #             i = i.rstrip('$')
+    #             i = i.replace('\\','')
+    #             final = 'DOMAIN-SUFFIX,{domain},PROXY\n'.format(domain=i)
+    #             tmp.append(final)
+    #         else:
+    #             final = 'DOMAIN-KEYWORD,{domain},PROXY\n'.format(domain=i)
+    #             tmp.append(final)
+    # with open('domain.conf','w') as f:
+    #     f.writelines(tmp)
+    
+    # 生成CIDR代理
+    with open('ipProxy.txt') as f:
         for i in f.readlines():
             i = i.strip('\n')
-            final = 'IP-CIDR,{ip},REJECT\n'.format(ip=i)
+            if i == '':
+                continue
+            final = 'IP-CIDR,{ip},PROXY\n'.format(ip=i)
             tmp.append(final)
-    with open('reject.conf','w') as f:
+
+    with open('ip.conf','w') as f:
         f.writelines(tmp)
